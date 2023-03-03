11dc2d02cd19d5059b815f258d2bc58eed812581
Jerry <gerardofv02@gmail.com>
2023-03-03 12:48:34 +0100

    Updated

---
+++ Next3.0/clase3/src/components/Formulario.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+
+const Formulario = () => {
+    const [data, setData] = useState<string[]>([])
+    const [nombre, setNombre] = useState<string>("")
+    const [page, setPage] = useState<number>(1) 
+    useEffect(() => {
+        const fetchData = async () => {
+            const chars = await fetch(`https://rickandmortyapi.com/api/character/?name=${nombre}&page=${page}`);
+            const json = await chars.json();
+            const names = json.results.map((char: any) => char.name);
+            setData(names);
+        }
+        try{
+        fetchData();
+    }catch(e){
+        setData(["Error al buscar"]);
+    }
+        
+    }
+        , [nombre,page])
+
+
+    if (data.length === 0) {
+        return <>Loading data</>
+    }
+
+    return (
+        <>
+           Introduce nombre : <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)}/> <br/>
+           Introduce Pagina : <input type="number" value={page} onChange={(e) => setPage(parseInt(e.target.value))}/> <br/>
+           <button onClick={() => setPage(page+1)}>Siguiente página</button>
+           <button onClick={() => setPage(page-1)}>Pagina anterior </button>
+            {data.map(item => (<div>{item}</div>))}
+        </>
+    )
+}
+
+export default Formulario;
